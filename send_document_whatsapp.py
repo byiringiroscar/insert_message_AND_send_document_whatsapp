4dba0acaec7834e8a05d457b50325c979625fb96
unknown <byiringoroscar@gmail.com>
2022-09-23 14:24:09 +0200

    fix null

--- send_document_whatsapp.py
+++ send_document_whatsapp.py
@@ -8,25 +8,28 @@ API_TOKEN = config('API_TOKEN')
 WHATSAPP_ID = config('WHATSAPP_ID')
 INSTANCE = config('INSTANCE')
 
-path_file = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.csv') or f.endswith('.pdf') or f.endswith('.xlsx')]
+path_file = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.csv') or f.endswith('.pdf') or f.endswith('.xlsx') or f.endswith('.xls')]
 
 
-for path_file in path_file:
-    file_complete = os.path.splitext(path_file)
-    file_name = ''
-    for file in file_complete:
-        file_name += file
-    print(file_name)
-    with open(path_file, "rb") as fileInDirectory:
-        encoded_string = base64.b64encode(fileInDirectory.read())
-    img_bas64 = urllib.parse.quote_plus(encoded_string)
-    url = f"https://api.ultramsg.com/{INSTANCE}/messages/document"
+if path_file:
+    for path_file in path_file:
+        file_complete = os.path.splitext(path_file)
+        file_name = ''
+        for file in file_complete:
+            file_name += file
+        print(file_name)
+        with open(path_file, "rb") as fileInDirectory:
+            encoded_string = base64.b64encode(fileInDirectory.read())
+        img_bas64 = urllib.parse.quote_plus(encoded_string)
+        url = f"https://api.ultramsg.com/{INSTANCE}/messages/document"
 
-    payload = f"token={API_TOKEN}&to={WHATSAPP_ID}&filename={file_name}&document={img_bas64}&referenceId=&nocache="
-    headers = {'content-type': 'application/x-www-form-urlencoded'}
+        payload = f"token={API_TOKEN}&to={WHATSAPP_ID}&filename={file_name}&document={img_bas64}&referenceId=&nocache="
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
 
-    try:
-        response = requests.request("POST", url, data=payload, headers=headers)
-    except ConnectionError as e:
-        print(e)
-        print("---------------network freezing -----------error ---------")
+        try:
+            response = requests.request("POST", url, data=payload, headers=headers)
+        except ConnectionError as e:
+            print(e)
+            print("---------------network freezing -----------error ---------")
+else:
+    print(" ====================no matched file availabe ====================")
